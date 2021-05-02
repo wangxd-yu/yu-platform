ef222dfd21a58af5049cd54719541881b449d218
wxd <657716219@qq.com>
2021-05-02 17:27:27 +0800

    feat: init yu-redis-spring-boot-starter 模块

---
+++ yu/yu-serve/serve-common/yu-redis-spring-boot-starter/src/main/java/org/yu/common/redis/RedisAutoConfigure.java
@@ -0,0 +1,89 @@
+package org.yu.common.redis;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.time.Duration;
+
+/**
+ * redis配置类
+ *
+ * @author wangxd
+ * @date 2021-03-16 17:56
+ */
+@EnableCaching
+@EnableConfigurationProperties({RedisProperties.class})
+public class RedisAutoConfigure {
+    @Bean
+    public RedisSerializer<String> redisKeySerializer() {
+        return RedisSerializer.string();
+    }
+
+    @Bean
+    public RedisSerializer<Object> redisValueSerializer() {
+        return RedisSerializer.json();
+    }
+
+    /**
+     * RedisTemplate配置
+     *
+     * @param factory RedisConnectionFactory
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(@Qualifier("redissonConnectionFactory") RedisConnectionFactory factory
+            , RedisSerializer<String> redisKeySerializer, RedisSerializer<Object> redisValueSerializer) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+
+        redisTemplate.setDefaultSerializer(redisValueSerializer);
+        redisTemplate.setKeySerializer(redisKeySerializer);
+        redisTemplate.setHashKeySerializer(redisKeySerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    @Primary
+    @Bean(name = "cacheManager")
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory
+            , RedisSerializer<String> redisKeySerializer, RedisSerializer<Object> redisValueSerializer) {
+        RedisCacheConfiguration difConf = getDefConf(redisKeySerializer, redisValueSerializer).entryTtl(Duration.ofHours(1));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(difConf)
+                //.withInitialCacheConfigurations(redisCacheConfigurationMap)
+                .build();
+    }
+
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return (target, method, objects) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getName());
+            sb.append(":").append(method.getName()).append(":");
+            for (Object obj : objects) {
+                sb.append(obj.toString());
+            }
+            return sb.toString();
+        };
+    }
+
+    private RedisCacheConfiguration getDefConf(RedisSerializer<String> redisKeySerializer, RedisSerializer<Object> redisValueSerializer) {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues()
+                .computePrefixWith(cacheName -> "cache".concat(":").concat(cacheName).concat(":"))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisKeySerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisValueSerializer));
+    }
+}
