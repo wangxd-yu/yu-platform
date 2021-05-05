cfbb7280fce3390b5e593e9b78045b554bba0472
wxd <657716219@qq.com>
2021-05-05 21:35:48 +0800

    feat: serve-gateway dev

---
+++ yu/yu-serve/serve-gateway/src/main/java/org/yu/serve/gateway/config/IgnoreUrlsConfig.java
@@ -0,0 +1,20 @@
+package org.yu.serve.gateway.config;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 网关白名单配置
+ * Created by yu on 2020/6/17.
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Component
+@ConfigurationProperties(prefix = "secure.ignore")
+public class IgnoreUrlsConfig {
+    private List<String> urls;
+}
