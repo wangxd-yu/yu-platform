876dc97707d86d3b52518ab547e8d72aea90a834
wxd <657716219@qq.com>
2021-05-11 21:13:24 +0800

    feat: serve-auth config&component

---
+++ yu/yu-serve/serve-auth/src/main/java/org/yu/serve/auth/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package org.yu.serve.auth.config;
+
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * SpringSecurity配置
+ *
+ * @author wangxd
+ * @date 2020/6/19
+ */
+@Configuration
+@EnableWebSecurity
+@Import(DefaultPasswordConfig.class)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+                .antMatchers("/rsa/publicKey").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
