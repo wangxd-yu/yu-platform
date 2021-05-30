4087821f35fc6ee2e8a970e40a6c7c5bfb310793
wxd <657716219@qq.com>
2021-05-30 22:29:51 +0800

    u

---
+++ yu/yu-serve/serve-system/src/main/java/org/yu/serve/system/module/dict/domain/DictDO.java
@@ -0,0 +1,31 @@
+package org.yu.serve.system.module.dict.domain;
+
+import lombok.Data;
+import org.yu.common.querydsl.domain.DslBaseTenantDO;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author wangxd
+ * @date 2020-11-27 14:06
+ */
+@Data
+@Entity
+@Table(name = "sys_dict")
+public class DictDO extends DslBaseTenantDO<Long> {
+    /**
+     * 编号
+     */
+    private String code;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 备注
+     */
+    private String remark;
+}
