be0dcbd4082a8e898e6151f13b31b050f40bd3a1
wxd <657716219@qq.com>
2021-06-03 21:10:16 +0800

    feat: serve-system menu 的 controller

---
+++ yu/yu-serve/serve-system/src/main/java/org/yu/serve/system/module/menu/controller/MenuController.java
@@ -0,0 +1,59 @@
+package org.yu.serve.system.module.menu.controller;
+
+import cn.hutool.core.collection.CollectionUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.yu.common.core.context.YuContextHolder;
+import org.yu.common.querydsl.controller.DslBaseApiController;
+import org.yu.serve.system.module.menu.domain.MenuDO;
+import org.yu.serve.system.module.menu.dto.MenuDTO;
+import org.yu.serve.system.module.menu.query.MenuQuery;
+import org.yu.serve.system.module.menu.service.MenuService;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author wangxd
+ * @date 2020-11-26 14:44
+ */
+@RestController
+@RequestMapping("menu")
+public class MenuController extends DslBaseApiController<MenuService, MenuDO, Long> {
+
+    private final MenuService menuService;
+
+    public MenuController(MenuService menuService) {
+        super(menuService);
+        this.menuService = menuService;
+    }
+
+    @GetMapping("build")
+    public ResponseEntity<Object> buildMenus() {
+        List<String> roleCodes = YuContextHolder.getYuContext().getClientUser().getRoles();
+        if (CollectionUtil.isEmpty(roleCodes)) {
+            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.OK);
+        }
+        List<MenuDTO> menus = menuService.findByRoleCodes(roleCodes);
+        List<MenuDTO> menuDTOTree = (List<MenuDTO>) menuService.buildTree(menus).get("content");
+        return new ResponseEntity<>(menuService.buildMenus(menuDTOTree), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Map<?, ?>> getMenus(MenuQuery criteria) {
+        List<MenuDTO> menuDTOS = (List<MenuDTO>) menuService.queryDTO(criteria, null, MenuDTO.class);
+        return new ResponseEntity<>(menuService.buildTree(menuDTOS), HttpStatus.OK);
+    }
+
+    /**
+     * 返回全部的菜单
+     */
+    @GetMapping("tree")
+    public ResponseEntity<Object> getMenuTree() {
+        return new ResponseEntity<>(menuService.getMenuTree(menuService.findByPid(0L)), HttpStatus.OK);
+    }
+}
