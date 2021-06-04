a7951a8022a182822c8ccbf30c0bec7afb1d6e14
wxd <657716219@qq.com>
2021-06-04 20:48:28 +0800

    feat: serve-system dict 的 domain

---
+++ yu/yu-serve/serve-system/src/main/java/org/yu/serve/system/module/dict/domain/DictDetailDO.java
@@ -0,0 +1,41 @@
+package org.yu.serve.system.module.dict.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.yu.common.querydsl.domain.DslBaseTenantDO;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author wangxd
+ * @date 2020-11-27 14:06
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+@Table(name = "sys_dict_detail")
+public class DictDetailDO extends DslBaseTenantDO<Long> {
+    /**
+     * 字典id
+     */
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long dictId;
+
+    /**
+     * 编号
+     */
+    private String code;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 排序
+     */
+    private Integer sort;
+}
