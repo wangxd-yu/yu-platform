6f36d3a179bbac2896eb78cff895a5f95df6c7a0
wxd <657716219@qq.com>
2021-06-07 17:47:32 +0800

    feat: serve-system dict 的 repository

---
+++ yu/yu-serve/serve-system/src/main/java/org/yu/serve/system/module/dict/repository/DictDetailRepository.java
@@ -0,0 +1,11 @@
+package org.yu.serve.system.module.dict.repository;
+
+import org.yu.serve.system.module.dict.domain.DictDetailDO;
+import org.yu.common.querydsl.repository.DslBaseRepository;
+
+/**
+ * @author wangxd
+ * @date 2020-11-27 15:05
+ */
+public interface DictDetailRepository extends DslBaseRepository<DictDetailDO, Long> {
+}
---
+++ yu/yu-serve/serve-system/src/main/java/org/yu/serve/system/module/dict/repository/DictRepository.java
@@ -0,0 +1,11 @@
+package org.yu.serve.system.module.dict.repository;
+
+import org.yu.serve.system.module.dict.domain.DictDO;
+import org.yu.common.querydsl.repository.DslBaseRepository;
+
+/**
+ * @author wangxd
+ * @date 2020-11-27 14:30
+ */
+public interface DictRepository extends DslBaseRepository<DictDO, Long> {
+}
