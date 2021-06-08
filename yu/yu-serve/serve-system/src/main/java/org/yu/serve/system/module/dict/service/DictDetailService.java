e69b91e047bc101b7773dd298459e25b8cceaa13
wxd <657716219@qq.com>
2021-06-08 17:46:51 +0800

    feat: serve-system dict 的 service

---
+++ yu/yu-serve/serve-system/src/main/java/org/yu/serve/system/module/dict/service/DictDetailService.java
@@ -0,0 +1,12 @@
+package org.yu.serve.system.module.dict.service;
+
+import org.yu.serve.system.module.dict.domain.DictDetailDO;
+import org.yu.common.querydsl.service.DslBaseService;
+
+/**
+ * @author wangxd
+ * @date 2020-11-27 15:05
+ */
+public interface DictDetailService extends DslBaseService<DictDetailDO, Long> {
+    void deleteByDictId(Long dictId);
+}
---
+++ yu/yu-serve/serve-system/src/main/java/org/yu/serve/system/module/dict/service/DictService.java
@@ -0,0 +1,11 @@
+package org.yu.serve.system.module.dict.service;
+
+import org.yu.serve.system.module.dict.domain.DictDO;
+import org.yu.common.querydsl.service.DslBaseService;
+
+/**
+ * @author wangxd
+ * @date 2020-11-27 14:32
+ */
+public interface DictService extends DslBaseService<DictDO, Long> {
+}
