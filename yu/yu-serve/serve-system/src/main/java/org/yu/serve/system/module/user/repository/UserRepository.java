1a699af8ef6aa903d07f343d930776c8ad42264c
wxd <657716219@qq.com>
2021-06-14 20:37:09 +0800

    feat: serve-system user 的 repository

---
+++ yu/yu-serve/serve-system/src/main/java/org/yu/serve/system/module/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package org.yu.serve.system.module.user.repository;
+
+import org.yu.serve.system.module.user.domain.UserDO;
+import org.yu.common.querydsl.repository.DslBaseRepository;
+
+/**
+ * @author wangxd
+ * @date 2020-11-09 10:00
+ */
+public interface UserRepository extends DslBaseRepository<UserDO, Long> {
+}
