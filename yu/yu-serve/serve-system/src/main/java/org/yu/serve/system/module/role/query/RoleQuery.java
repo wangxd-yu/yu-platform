9b115e4bc49e924e7bde937286fbd181523f53e2
wangxd_yu <657716219@qq.com>
2021-08-09 23:32:23 +0800

    fix: 查询类实现

--- yu/yu-serve/serve-system/src/main/java/org/yu/serve/system/module/role/query/RoleQuery.java
+++ yu/yu-serve/serve-system/src/main/java/org/yu/serve/system/module/role/query/RoleQuery.java
@@ -2,6 +2,8 @@ package org.yu.serve.system.module.role.query;
 
 import lombok.Data;
 import org.yu.common.querydsl.query.annotation.YuQuery;
+import org.yu.common.querydsl.query.annotation.YuQueryColumn;
+import org.yu.common.querydsl.query.enums.YuOperatorEnum;
 import org.yu.serve.system.module.role.domain.RoleDO;
 
 /**
@@ -11,4 +13,9 @@ import org.yu.serve.system.module.role.domain.RoleDO;
 @Data
 @YuQuery(domain = RoleDO.class)
 public class RoleQuery {
+    @YuQueryColumn(operator = YuOperatorEnum.INNER_LIKE)
+    private String name;
+
+    @YuQueryColumn(operator = YuOperatorEnum.INNER_LIKE)
+    private String code;
 }
