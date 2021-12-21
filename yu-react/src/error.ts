caf01640b3616b619c3f17a5ffac59e801ce19e1
wangxd-yu <657716219@qq.com>
2021-12-21 22:53:04 +0800

    fix: 前端error基类

---
+++ yu-react/src/error.ts
@@ -0,0 +1,18 @@
+class BaseError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = new.target.name;
+        if (typeof (Error as any).captureStackTrace === 'function') {
+            (Error as any).captureStackTrace(this, new.target);
+        }
+        if (typeof Object.setPrototypeOf === 'function') {
+            Object.setPrototypeOf(this, new.target.prototype);
+        } else {
+            (this as any).__proto__ = new.target.prototype;
+        }
+    }
+}
+
+class InterceptorError extends BaseError { }
+
+export { BaseError, InterceptorError };
