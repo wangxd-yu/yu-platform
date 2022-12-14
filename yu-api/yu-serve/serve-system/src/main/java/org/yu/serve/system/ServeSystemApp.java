bf8b98c1fd6e820cc56c77c4fae80403d872bc5d
wangxd-yu <657716219@qq.com>
2022-12-14 11:17:04 +0800

    fix: 删除 cloud 依赖

--- yu-api/yu-serve/serve-system/src/main/java/org/yu/serve/system/ServeSystemApp.java
+++ yu-api/yu-serve/serve-system/src/main/java/org/yu/serve/system/ServeSystemApp.java
@@ -2,16 +2,12 @@ package org.yu.serve.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.yu.common.multidb.config.EnableMultiDatabase;
 
 /**
  * @author wangxd
  */
-@EnableMultiDatabase
 @EnableJpaAuditing
-@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"org.yu.serve.system"})
 public class ServeSystemApp {
     public static void main(String[] args) {
